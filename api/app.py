af972145e96eec3e26693c76c98089df17e6b583
Santiago Basulto <santiago.basulto@gmail.com>
2017-06-05 18:17:17 -0300

    CORS Headers

--- api/app.py
+++ api/app.py
@@ -8,16 +8,24 @@ from .conf import LANGUAGES_CONF
 app = Flask(__name__)
 
 
-@app.route('/execute', methods=['POST'])
+@app.route('/execute', methods=['POST', 'OPTIONS'])
 @json_parameters({
     'required': ['language'],
     'optional': ['code', 'command', 'flavor', 'files', 'produces'],
     'at_least_one_of': ['command', 'code']
 })
 def execute(language, **kwargs):
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type'
+    }
+    if request.method == 'OPTIONS':
+        return json_response("", headers=headers)
+
     try:
         command = executor.ExecutorCommand(LANGUAGES_CONF)
         result = command.execute(language, **kwargs)
-        return json_response(result)
+        return json_response(result, headers=headers)
     except executor.exceptions.InvalidLanguageFlavorException:
         return error_response('Language/flavor is invalid')
